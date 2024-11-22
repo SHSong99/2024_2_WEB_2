2fa17c629cf4b9012229d0e26b31a644f565de00
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-11-23 00:44:44 +0900

    [Server] feature : 투표 기능 구현 (#128) (#137)
    * fix: 프로젝트의 vote가 기본값을 가지도록 수정
    
    [Project]
    - vote에 Nullable=false 를 추가
    
    [ProjectCreateRequest]
    - toEntity()에서 vote를 0L으로 초기화
    
    * feat: User Entity에 voted 항목 추가
    
    * feat: 투표 정보를 담는 DTO 생성
    
    * feat: 투표를 완료한 유저의 voted 상태를 업데이트 하는 기능 추가
    
    * feat: 프로젝트의 받은 투표 수를 업데이트하는 기능 추가
    
    * feat: 투표를 처리하는 기능 추가
    
    [VoteService]
    - processVote가 Transactional 하도록 어노테이션 추가
    - 투표를 이미 실시했으면 예외를 던져서 알림
    - vote를 통해 투표된 프로젝트의 vote를 업데이트
    - 투표에 성공하면 해당 유저의 voted 상태 업데이트
    
    * feat: 투표를 담당하는 컨트롤러 추가
    
    - 각 상황에 맞는 적절한 메시지와 함께 ResponseEntity를 반환한다.

--- server/src/main/java/wap/web2/server/repository/ProjectRepository.java
+++ server/src/main/java/wap/web2/server/repository/ProjectRepository.java
@@ -2,6 +2,7 @@ package wap.web2.server.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import wap.web2.server.domain.Project;
@@ -14,7 +15,10 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     @Query("SELECT b FROM Project b WHERE b.projectId = :projectId AND b.user.id = :userId")
     Project findByProjectIdAndUser(@Param("projectId") Long projectId, @Param("userId") Long userId);
 
+    @Modifying
+    @Query("UPDATE Project p SET p.vote = p.vote + 1 WHERE p.projectId = :projectId")
+    int voteByProjectId(@Param("projectId") Long projectId);
+
     @Query("SELECT p FROM Project p WHERE p.projectYear = :year AND p.semester = :semester ORDER BY p.projectId DESC")
     List<Project> findProjectsByYearAndSemesterOrderByProjectIdDesc(@Param("year") Long year, @Param("semester") Long semester);
-
 }
