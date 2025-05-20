5c6f2318aa15f39c2187d89cb8e9e4633052ff99
kangrae <kangrae0501@gmail.com>
2025-05-20 11:23:03 +0900

    fix: 코멘트 기능은 로그인한 유저만 요청 가능하도록 수정
    - 컨트롤러에서 토큰을 요구하도록하여 기능 구현

--- server/src/main/java/wap/web2/server/comment/controller/CommentController.java
+++ server/src/main/java/wap/web2/server/comment/controller/CommentController.java
@@ -3,10 +3,17 @@ package wap.web2.server.comment.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import wap.web2.server.comment.dto.request.CommentCreateRequest;
 import wap.web2.server.comment.dto.request.CommentDeleteRequest;
 import wap.web2.server.comment.service.CommentService;
+import wap.web2.server.ouath2.security.CurrentUser;
+import wap.web2.server.ouath2.security.UserPrincipal;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,14 +23,16 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/{projectId}")
-    public ResponseEntity<?> createComment(@PathVariable Long projectId, @RequestBody CommentCreateRequest request) {
-        commentService.save(projectId, request);
+    public ResponseEntity<?> createComment(@PathVariable Long projectId, @RequestBody CommentCreateRequest request,
+                                           @CurrentUser UserPrincipal userPrincipal) {
+        commentService.save(projectId, request, userPrincipal);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<String> deleteBook(@PathVariable Long commentId, @RequestBody CommentDeleteRequest request) {
-        boolean isDeleted = commentService.deleteCommentByPassword(commentId, request);
+    public ResponseEntity<String> deleteBook(@PathVariable Long commentId, @RequestBody CommentDeleteRequest request,
+                                             @CurrentUser UserPrincipal userPrincipal) {
+        boolean isDeleted = commentService.deleteCommentByPassword(commentId, request, userPrincipal);
         if (isDeleted) {
             return ResponseEntity.ok("댓글이 삭제되었습니다.");
         } else {
--- server/src/main/java/wap/web2/server/comment/service/CommentService.java
+++ server/src/main/java/wap/web2/server/comment/service/CommentService.java
@@ -1,16 +1,16 @@
 package wap.web2.server.comment.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import wap.web2.server.comment.entity.Comment;
-import wap.web2.server.project.entity.Project;
 import wap.web2.server.comment.dto.request.CommentCreateRequest;
 import wap.web2.server.comment.dto.request.CommentDeleteRequest;
+import wap.web2.server.comment.entity.Comment;
 import wap.web2.server.comment.repository.CommentRepository;
+import wap.web2.server.ouath2.security.UserPrincipal;
+import wap.web2.server.project.entity.Project;
 import wap.web2.server.project.repository.ProjectRepository;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,7 +20,7 @@ public class CommentService {
     private final ProjectRepository projectRepository;
 
     @Transactional
-    public void save(Long projectId, CommentCreateRequest request) {
+    public void save(Long projectId, CommentCreateRequest request, UserPrincipal userPrincipal) {
         Project project = projectRepository.findById(projectId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 프로젝트"));
 
@@ -30,7 +30,7 @@ public class CommentService {
     }
 
     @Transactional
-    public boolean deleteCommentByPassword(Long commentId, CommentDeleteRequest request) {
+    public boolean deleteCommentByPassword(Long commentId, CommentDeleteRequest request, UserPrincipal userPrincipal) {
         Optional<Comment> optionalBook = commentRepository.findById(commentId);
         if (optionalBook.isPresent()) {
             Comment comment = optionalBook.get();
