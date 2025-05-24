98bd69b80f8b42b88d570535ef78a2ea03d16b9c
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-25 00:56:54 +0900

    Modify/#248 (#254)
    * feature : 투표한 유저일 때 투표한 페이지 가져오는 함수 정의
    
    * fix : 이미지 여러장 삭제되는 오류 해결

--- client/src/pages/VotePage.js
+++ client/src/pages/VotePage.js
@@ -12,11 +12,15 @@ import VoteResultPage from "./VoteResultPage";
 // 분기를 결정함.
 // 현재 투표 기간인지에 따라 분기 구별함.
 const VotePage = () => {
+  const currentYear = new Date().getFullYear(); // 현재 연도 가져오기
   // 분기를 결정하는 api 임.
-  const apiUrl = `${process.env.REACT_APP_API_BASE_URL}/vote/now`;
+  const apiUrl = `${process.env.REACT_APP_API_BASE_URL}/vote/now?semester=2&projectYear=${currentYear}`;
   const token = Cookies.get("authToken");
   const [menuOpen, setMenuOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+
+  // 투표했는지 여부의 초기값은 일단 false
+  const [isvotedUser, setIsVotedUser] = useState(false);
   const navigate = useNavigate();
 
   const toggleMenu = () => {
@@ -56,6 +60,10 @@ const VotePage = () => {
         // 가져온 데이터 표시
         // console.log("프로젝트 상세 정보:", response.data);
         console.log(response.data.isOpen);
+
+        // 받은 boolean 값을 set으로 설정해줌.
+        setIsOpen(response.data.isOpen);
+        setIsVotedUser(response.date.isvotedUser);
       } catch (error) {
         alert("투표기간인지 확인할 수 없습니다. ");
       } finally {
@@ -71,8 +79,10 @@ const VotePage = () => {
 
       <Menu menuOpen={menuOpen} toggleMenu={toggleMenu} />
       <main>
-        {isOpen ? <VoteForm /> : <VoteResultPage />}
-        {/* <VoteForm /> */}
+        {/* 이게 내가 사용해야할 코드임!! 지금 백엔드가 안되어있어서 임시방편으로
+        아래방법 선택함. */}
+        {/* {isOpen ? <VoteForm /> : <VoteResultPage />} */}
+        <VoteForm isVotedUser={isvotedUser} />
       </main>
       <FloatingButton />
     </div>
